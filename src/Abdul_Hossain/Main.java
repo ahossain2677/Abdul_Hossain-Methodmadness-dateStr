b96a947e30f70c32c7586cafdf03020f4aa2f6fb
BT_1N3_08@BKTECH.LOCAL <BT_1N3_08@BKTECH.LOCAL>
2018-10-01 08:42:08 -0400

    Initial commit

---
+++ src/Abdul_Hossain/Main.java
@@ -0,0 +1,12 @@
+package Abdul_Hossain;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out. println (output)
+    }
+    public static String datStr( String str )
+
+
+}
