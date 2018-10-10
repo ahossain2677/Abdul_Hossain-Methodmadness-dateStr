aeca68c1dc6f3c775b53d3e3ee665f307e9d3920
ahossain2677 <43207698+ahossain2677@users.noreply.github.com>
2018-10-09 21:51:20 -0400

    Update Main.java

--- src/Abdul_Hossain/Main.java
+++ src/Abdul_Hossain/Main.java
@@ -1,12 +1,16 @@
 package Abdul_Hossain;
 
-public class Main {
-
-    public static void main(String[] args) {
-
-        System.out. println (output)
+public class dateStr
+{
+    public static void dateStr (String[] args)
+    {
+        String (String x);
+        {
+            String x= x.substring (3,4); //days
+            String x= x.substring (0,1); //months
+            String x =x.substring (6,9);// years
+        }
+        System.out.println( "x" + "-" + "y" + "-" + "z");
     }
-    public static String datStr( String str )
-
-
 }
+
